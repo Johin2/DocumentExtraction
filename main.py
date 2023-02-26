c35d34bb474d138b4df8337e6b3d69cf210e0c04
Johin <johinjohny144@gmail.com>
2023-02-26 11:38:40 +0530

    Did user Validation so that if a the proper file path is not entered the programs throws a Exception

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 
 
 def upload_file(file_path):
-    try:
-        img = cv2.imread(file_path)
-        height, width = img.shape[:2]
-        img = cv2.resize(img, (width // 2, height // 2))
-        cv2.imshow("image", img)
-        cv2.waitKey(0)
+    if not os.path.isfile(file_path):
+       raise FileNotFoundError ("File not found")
 
-        text = pytesseract.image_to_string(img, lang='eng', config='--psm 4')
-        print("OCR Output:\n", text)
+    img = cv2.imread(file_path)
+    height, width = img.shape[:2]
+    img = cv2.resize(img, (width // 2, height // 2))
+    cv2.imshow("image", img)
+    cv2.waitKey(0)
 
-    except FileNotFoundError as e:
-        print("File not found, Please upload the file again")
+    text = pytesseract.image_to_string(img, lang='eng', config='--psm 4')
+    print("OCR Output:\n", text)
 
 
 def insert_to_database(file_path, document_type, cnx):
@@ -24,8 +23,11 @@ def insert_to_database(file_path, document_type, cnx):
         img_data = img.read()
 
         cursor = cnx.cursor()
-        cursor.execute("INSERT INTO documents (Name, image, document_type) VALUES (%s, %s,%s)",
-                       (file_path, img_data, document_type))
+        stmt = "INSERT INTO documents (Name, image, document_type) VALUES (%s, %s,%s)",
+        for file_path in file_paths:
+            with open(file_path, 'rb') as img:
+                img_data = img.read()
+                cursor.execute(stmt,(file_path,img_data, document_type))
         cnx.commit()
         cursor.close()
 
@@ -43,41 +45,51 @@ def upload_documents():
             quit_flag = True
             break
 
-        file_paths = input(f"Enter file paths for {document_type} (comma separated): ").split(",")
-        for file_path in file_paths:
-            if file_path in uploaded_paths:
-                print("This file has already been uploaded. Please upload a different file.")
-                continue
+        while True:
+            file_paths = input(f"Enter file paths for {document_type} (comma separated): ").split(",")
+            if all([os.file.isfile(file_path) for file_path in file_paths]):
+                break
+            else:
+                print("One or more file not found. Please try again.")
+
+            for file_path in file_paths:
+                if file_path in uploaded_paths:
+                    print("This file has already been uploaded. Please upload a different file.")
+                    continue
 
             uploaded_paths.append(file_path)
-            upload_file(file_path)
+            try:
+                upload_file(file_path)
+            except FileNotFoundError:
+                print(f"{file_path} not found. Skipping Upload")
+                continue
             documents[document_type].append(file_path)
 
-
-    return uploaded_paths,documents,quit_flag
+    return uploaded_paths, documents, quit_flag
 
 
 def main():
     uploaded_paths, documents, quit_flag = upload_documents()
 
     # Connecting to my Sql Server
-    cnx = mysql.connector.connect(
+    with mysql.connector.connect(
         host="localhost",
         user="root",
         password="Johin2004!",
         database="documents"
-    )
+    ) as cnx:
 
-    if not quit_flag:
-       for document_type, file_paths in documents.items():
-             for file_path in file_paths:
-                 insert_to_database(file_path, document_type, cnx)
+        if not quit_flag:
+            for document_type, file_paths in documents.items():
+                for file_path in file_paths:
+                    insert_to_database(file_path, document_type, cnx)
 
-       print("Images Uploaded successfully")
-    else:
-        print("Image uploading cancelled by user")
+            print("Images Uploaded successfully")
+        else:
+            print("Image uploading cancelled by user")
 
-    cnx.close()
+        cnx.close()
+
 
 
 if __name__ == "__main__":
